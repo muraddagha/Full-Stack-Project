b01b1d33479630f2d1d95af3ad5383e7f7ea60b0
muraddagha <muradha@code.edu.az>
2021-05-17 21:42:19 +0400

    Contact page added

--- Front/final-project/src/app/shared/models/setting.model.ts
+++ Front/final-project/src/app/shared/models/setting.model.ts
@@ -1,8 +1,6 @@
 export interface ISetting {
-    id: number
     adress: string
     phone: string
     email: string
     logo: string
-    fileName: any
 }
--- Front/final-project/src/app/views/app/contact/contact.component.ts
+++ Front/final-project/src/app/views/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ISetting } from 'src/app/shared/models/setting.model';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public setting: ISetting;
+  constructor(private apiService: ApiService,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.getUserAdress();
+  }
+  private getUserAdress(): void {
+    this.apiService.getSetting().subscribe(res => {
+      this.setting = res;
+    })
   }
 
 }
